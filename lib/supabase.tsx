31fbe04095b97a1706264919a9d40483adee55ac
Giovanni Milan <125603962+Giovannip11@users.noreply.github.com>
2024-11-15 11:21:57 -0300

    Delete lib directory

--- lib/supabase.tsx
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://nicvgxdupyqabzggjoxl.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY as string
-const supabase = createClient(supabaseUrl, supabaseKey)
